b2d55a2d9c387fd4782e91fc1a57e3101cd09184
prateek pi <ptk1917@gmail.com>
2025-06-27 19:02:54 +0000

    fixes

--- ros_launch/launch_ros.sh
+++ ros_launch/launch_ros.sh
@@ -3,11 +3,10 @@
 source /opt/ros/noetic/setup.bash
 source ~/catkin_ws/devel/setup.bash
 
-# 2) if roscore isn’t already running, start it in the background
-if ! pgrep -x roscore > /dev/null; then
-  roscore > /home/pi/roscore.log 2>&1 &
-  sleep 2    # give it a moment to spin up
+if pgrep -x roscore > /dev/null; then
+  echo "roscore is already up. Don't need to start"
 fi
+
 
 # 3) replace this shell with your Velodyne launch
 exec roslaunch velodyne_pointcloud VLP16_points.launch
